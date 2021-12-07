42694606f374b73ff816a02862bbe5c076711ecf
Cadenza <mirandalucsb@gmail.com>
2021-12-06 20:07:21 -0700

    finished functions for quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@
 //health (player and opponent)
 //game over screen
 //screen to enter initials for the high score
-
+var quizContainerEl = document.querySelector(".game-container");
+var mainEl = document.querySelector("main");
 class answer {
     text;
     isCorrect;
@@ -18,6 +19,8 @@ class answer {
 class question{
     text;
     answers = [];
+    pointValueForCorrectAnswer;
+    pointValueForIncorrectAnswer;
 }
 
 var quizQuestions = [];
@@ -25,11 +28,18 @@ var quizQuestions = [];
 
 
 //object that keeps track of game's state
-class gameState{
-    playerHealth;
-    opponentHealth;
-    opponentAttack;
-    questionCounter;
+var gameState  = {
+    playerHealth : 50,
+    opponentHealth : 0,
+    opponentAttack : 0,
+    questionCounter : 1,
+}
+
+var resetContainerElement = function(){
+    quizContainerEl.remove();
+    quizContainerEl = document.createElement("section");
+    quizContainerEl.className = "game-container";
+    mainEl.appendChild(quizContainerEl);
 }
 
 var buildStartScreen = function(){
@@ -51,8 +61,27 @@ var buildInitialsScreen = function(){
     //save button - save initials and score to localStorage
 }
 
-var buildQuizQuestion = function(){
+var buildQuizQuestion = function(questionIndex){
     //generates HTML elements for quiz questions
+    console.log("build question", questionIndex);
+    resetContainerElement();
+    var h2El = document.createElement("h2");
+    h2El.textContent = "Question " + gameState.questionCounter + ": ";
+    quizContainerEl.appendChild(h2El);
+
+    var questionPEl = document.createElement("p");
+    questionPEl.textContent = quizQuestions[questionIndex].text;
+    questionPEl.className = "quiz-question"
+    quizContainerEl.appendChild(questionPEl);
+
+    for(var i =0; i<quizQuestions[questionIndex].answers.length; i++){
+        var answerPEl = document.createElement("p");
+        answerPEl.textContent = (i+1)+". " +quizQuestions[questionIndex].answers[i].text;
+        answerPEl.className = "quiz-answer";
+        quizContainerEl.appendChild(answerPEl);
+    }
+    gameState.questionCounter++;
+
 }
 
 var getOpponentImage = function(){
@@ -74,7 +103,34 @@ var getOpponentImage = function(){
     });
 }
 
-var quizQuestions = function(){
+var listQuizQuestions = function(){
     //add questions to the quiz here!
+    var q = new question();
+    q.text = "What does HTML stand for?"
+    q.pointValueForCorrectAnswer = 10;
+    q.pointValueForIncorrectAnswer = 20;
+    q.answers = [new answer("Hypertext Markup Language","correct"), new answer("Hypertext Mark Language","incorrect"), new answer("Hyphentext Markdown Language","incorrect"), new answer("Hypertext Markdown Language","incorrect")];
+    quizQuestions.push(q);
+
+    var q = new question();
+    q.text = "What does || stand for?"
+    q.pointValueForCorrectAnswer = 15;
+    q.pointValueForIncorrectAnswer  = 15;
+    q.answers = [new answer("And","incorrect"), new answer("Not","incorrect"), new answer("Or","correct"), new answer("Is Less Than","incorrect")];
+    quizQuestions.push(q);
+    console.log(quizQuestions);
 }
 
+
+
+listQuizQuestions();
+buildQuizQuestion(gameState.questionCounter-1);
+
+//add event listener to quiz answers on click
+
+mainEl.addEventListener("click",function(event){
+    if(event.target.className == "quiz-answer"){
+        console.log("answer clicked");
+        buildQuizQuestion(gameState.questionCounter-1);
+    } 
+});
